a952c1e329afeb8c0e8917fa621d5bcab9f51986
MR-SAJAN-SAH <157885911+MR-SAJAN-SAH@users.noreply.github.com>
2025-12-08 23:02:05 +0530

    Update RNN1.py

---
+++ RNN/RNN1.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layers=nn.Sequential(
+            nn.Linear(3,16),
+            nn.ReLU(),
+            nn.Linear(16,1)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
